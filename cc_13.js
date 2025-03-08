d1048e0821771f4419a69d4fbe11518757805ec5
Sahara Sipple <sahara36@usf.edu>
2025-03-08 08:23:09 -0500

    Task 3 - Bulk Update on Employee Cards and Added in More employee cards as test data

--- cc_13.js
+++ cc_13.js
@@ -21,4 +21,20 @@ function createEmployeeCard (name, position) {  // Function to create employee c
     employeeContainer.appendChild(card);
 }
 
-createEmployeeCard('Employee 1', 'Manager');
+createEmployeeCard('Employee 1', 'Store Director');  // Test Data Employee 1
+createEmployeeCard('Employee 2', 'Team Lead');  // Test Data Employee 2
+createEmployeeCard('Employee 3', 'Team Member');  // Test Data Employee 3
+
+// Task 3: Converting NodeLists to Arrays for Bulk Updates //
+
+function updateEmployeeCard () {  // Creates function to update employee cards
+    const employeeCards = document.querySelectorAll('.employee-card');  // selects every employee card
+    const arrayEmployeeCards = Array.from(employeeCards); // converts the nodeList to an array
+
+    arrayEmployeeCards.forEach(employeeCard => {  // for each employee card in the array
+        employeeCard.style.backgroundColor = '#90EE90'; // adds green background to each card
+        employeeCard.style.border = '2px solid #000000' // adds black border to each card
+    });
+}
+
+updateEmployeeCard(); // calls the function
