39e859c4378469340f358695f101c7c9b1973376
Sahara Sipple <sahara36@usf.edu>
2025-03-08 08:59:52 -0500

    Task 5 - Inline Editing for Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -19,6 +19,8 @@ function createEmployeeCard (name, position) {  // Function to create employee c
 
     const employeeContainer = document.getElementById('employeeContainer');
     employeeContainer.appendChild(card);
+    
+    // Task 3: See bottom of code! //
 
     // Task 4: Implementing Removal of Employee Cards with Event Bubbling //
     removeButton.addEventListener('click', (event) => { // adding event listener to the click 
@@ -29,6 +31,38 @@ function createEmployeeCard (name, position) {  // Function to create employee c
 
     employeeContainer.addEventListener('click', () => {  // adds event listener to click on the employee container
     console.log('Employee card was clicked!'); // Message that the employee card has been clicked
+    });
+
+    // Task 5: Inline Editing of Employee Details // 
+    
+    const editButton = document.createElement('button'); // creates edit button
+    editButton.textContent = 'Edit'; // creates text for the button as "Edit"
+    card.appendChild(editButton); // Appends the edit button to the card
+
+    editButton.addEventListener('click', () => {  // adds event listener to the edit Button'
+        const nameInput = document.createElement('input'); // creates input for name
+        nameInput.value = nameHeader.textContent; // pre-populates the edit field with the current name
+
+        const positionInput = document.createElement('input'); // creates input for position
+        positionInput.value = positionParagraph.textContent; // pre-populates the edit field with current position
+
+        const saveButton = document.createElement('button'); // creates save button
+        saveButton.textContent = 'Save'; // creates text "Save" for the button
+
+        card.replaceChild(nameInput, nameHeader); // replaces current text with updated text for name
+        card.replaceChild(positionInput, positionParagraph); // replaces current with updated for position
+        card.replaceChild(saveButton, editButton); // replaces the edit with the save button
+
+        saveButton.addEventListener('click', () => { // adds listener for clicking the save button
+            nameHeader.textContent = nameInput.value; // replaces name header with new input value
+            positionParagraph.textContent = positionInput.value; // replaces position with new input position
+
+            card.replaceChild(nameHeader, nameInput); // replaces input with updated info for name
+            card.replaceChild(positionParagraph, positionInput); // replaces input with updated info for position
+            card.replaceChild(editButton, saveButton); // replaces edit button with save button
+        
+        console.log('Employee details updated.'); // message to confirm updated details
+        });
     });
 }
 
